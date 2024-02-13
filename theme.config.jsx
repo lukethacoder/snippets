ad77b618b23cbe7bcd5ac6c00763ed29cf7f3b26
Luke Secomb <luke.secomb@live.com>
2024-02-14 10:12:26 +1100

    📝 add 'git' category / drop icon stroke width to 1

--- theme.config.jsx
+++ theme.config.jsx
@@ -8,7 +8,7 @@ const ICONS_FOR_TITLES = {
         width='24'
         height='24'
         viewBox='0 0 24 24'
-        strokeWidth='2'
+        strokeWidth='1'
         stroke='currentColor'
         fill='none'
         strokeLinecap='round'
@@ -29,7 +29,7 @@ const ICONS_FOR_TITLES = {
         width='24'
         height='24'
         viewBox='0 0 24 24'
-        strokeWidth='2'
+        strokeWidth='1'
         stroke='currentColor'
         fill='none'
         strokeLinecap='round'
@@ -49,7 +49,7 @@ const ICONS_FOR_TITLES = {
         width='24'
         height='24'
         viewBox='0 0 24 24'
-        strokeWidth='2'
+        strokeWidth='1'
         stroke='currentColor'
         fill='none'
         strokeLinecap='round'
@@ -86,6 +86,32 @@ const ICONS_FOR_TITLES = {
       <span>{title}</span>
     </span>
   ),
+  Git: (title) => (
+    <span className='nav-item'>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+        strokeWidth='1'
+        stroke='currentColor'
+        fill='none'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      >
+        <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+        <path d='M16 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
+        <path d='M12 8m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
+        <path d='M12 16m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
+        <path d='M12 15v-6' />
+        <path d='M15 11l-2 -2' />
+        <path d='M11 7l-1.9 -1.9' />
+        <path d='M13.446 2.6l7.955 7.954a2.045 2.045 0 0 1 0 2.892l-7.955 7.955a2.045 2.045 0 0 1 -2.892 0l-7.955 -7.955a2.045 2.045 0 0 1 0 -2.892l7.955 -7.955a2.045 2.045 0 0 1 2.892 0z' />
+      </svg>
+
+      <span>{title}</span>
+    </span>
+  ),
   Powershell: (title) => (
     <span className='nav-item'>
       <svg
@@ -93,7 +119,7 @@ const ICONS_FOR_TITLES = {
         width='24'
         height='24'
         viewBox='0 0 24 24'
-        strokeWidth='2'
+        strokeWidth='1'
         stroke='currentColor'
         fill='none'
         strokeLinecap='round'
